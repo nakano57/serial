e8253cd508cca6fed72470e25c544c7f9ab732ab
nakano57 <hiroto856@gmail.com>
2019-08-14 19:53:23 +0900

    change cu to screen

--- serial.sh
+++ serial.sh
@@ -1,4 +1,5 @@
 sudo chmod o+wr /dev/ttyUSB$1
-echo 'Input "~" + "." to disconnect'
-cu -l /dev/ttyUSB$1 -s 115200
-
+echo 'Press "control + a" and "k" to disconnect'
+echo 'When you need scroll, press "Ctrl-A" on the keyboard and press "Esc."'
+echo "Press "Esc" to exit scrollback mode."
+screen /dev/ttyUSB$1 115200
